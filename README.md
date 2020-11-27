26d4ecccb977dcbed138e870b017242166b5f94f
Abhisek Kumar Anand <anand5895@outlook.com>
2020-11-27 11:15:55 +0530

    minor additions

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Steps:
 3. install dependecies nodemon and express
 4. Add dev command to package.json (to enable : node run dev)
 5. start working on index.js and create web server.
-6. 
+6. ref express documentation to start a web server.
+7. Optional : use express generator to generate app skeleton.
+8.
--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const express = require("express");
-
 const app = express();
+const port = 3000;
 
-console.log("hi!");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/login", (req, res) => {
+  res.send("Login Page");
+});
+
+app.get("/signup", (req, res) => {
+  res.send("Signup Page");
+});
+
+app.get("/products", (req, res) => {
+  res.send("products Page");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
